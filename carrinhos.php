656fa9ffad53737079001cdba2f0115dae8f3231
sursum-git <tadeu.parreias@sursumcorda.com.br>
2022-09-08 13:31:59 -0300

    implementação função Carrinho

--- carrinhos.php
+++ carrinhos.php
@@ -1,16 +1,33 @@
 <?php 
 
-function montarFiltroPedido($aFiltros,$aApelidoTb)
+function montarFiltroCarrinho($aFiltros,$aApelidoTb)
 { /*********************************************** 
      chaves aFiltros: 
      log_rp,log_rd,it_codigo,lista_cod_refer
     *********************************************/
-    $aFiltroCond = array();
+    $aFiltroCond    = array();
+    $aFiltroLote    = array();
+    $aFiltroItem    = array();
+    $aFiltroRefer   = array();
     $tabela ='ob-etiqueta';
-    $aFiltroCond = inserirArrayCond($aFiltroCond,$tabela,'nr-pedido',$listaPedidos,'in',true);
-        
+    $rp          = $aFiltroCond['log_rp'];
+    $rd          = $aFiltroCond['log_rd'];
+    $itCodigo   = $aFiltroCond['it_codigo'];
+    $codRefer   = $aFiltroCond['lista_cod_refer'];
 
-    return convArrayToCondSql($aFiltroCond);
+
+    $aFiltroLote        = inserirArrayCond($aFiltroLote,$tabela,'lote',$rd,'=',false,true,'or');
+    $aFiltroLote        = inserirArrayCond($aFiltroLote,$tabela,'lote',$rp,'=',false,true,'or');
+
+
+    $aFiltroItem        = inserirArrayCond($aFiltroLote,$tabela,'it-codigo',$itCodigo,'=',false,true,'and');
+    $aFiltroCodRefer    = inserirArrayCond($aFiltroRefer,$tabela,'cod-refer',$codRefer,'=',false,true,'and');
+
+    $aFiltroCond        = inserirArrayCondMultiNivel($aFiltroCond,$aFiltroLote);
+    $aFiltroCond        = inserirArrayCondMultiNivel($aFiltroCond,$aFiltroItem);
+    $aFiltroCond        = inserirArrayCondMultiNivel($aFiltroCond,$aFiltroCodRefer);
+
+    return convArrayMultiToCondSql($aFiltroCond);
 }
 
 
